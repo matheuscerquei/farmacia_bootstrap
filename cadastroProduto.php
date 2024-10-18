73516a1af20c78d1e92893bd01003b2847c9138d
matheuscerquei <103291017+matheuscerquei@users.noreply.github.com>
2024-10-17 21:24:59 -0300

    termina dps

--- cadastroProduto.php
+++ cadastroProduto.php
@@ -2,71 +2,73 @@
 <html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="author" content="Augusto Mizu">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="shortcut icon" type="image/x-png" href="./imgens/icon_navegador2.png">
-  <title>Estoque</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="author" content="Augusto Mizu">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-png" href="./imgens/icon_navegador2.png">
+    <title>Estoque</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body style="margin-left: 25%; margin-right: 25%;">
-  <div id="nav_bar_padrao" style="margin-bottom: 100px;"></div>
-  <section class="border border-3 rounded p-5" style="border: rgba(255, 0, 0, .5);" >
-    <h1 style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">CADASTRO DE PRODUTOS</h1>
-    <form action="cadastroProduto_action.php" method="post">
-      <div class="mb-2 w-75">
-        <label for="nameInput">Nome do produto</label>
-        <input type="text" class="form-control" required>
-      </div>
-      <div class="mb-2 d-flex col">
-        <div class="w-50">
-          <label for="precoInput">Preço</label>
-          <input type="number" id="precoInput" step="0.01" min="0.01" class="form-control w-50" required>
-        </div>
-        <div class="w-50">
-          <label for="quantidadeInput">Quantidade</label>
-          <input type="number" id="quantidadeInput" step="1" min="1" class="form-control w-50" required>
-        </div>
-      </div>
-      <div class="mb-2 d-flex col">
-        <div class="w-50">
-          <label for="categoriaInput">Categoria</label>
-          <select name="categoriaSelect" id="categoriaSelect" class="form-control w-50">
-            <option value="" disabled selected>Selecione...</option>
-            <option value="ANALGÉSICOS">ANALGÉSICOS</option>
-            <option value="ANTI-INFLAMATÓRIOS">ANTI-INFLAMATÓRIOS</option>
-            <option value="ANTIBIÓTICOS">ANTIBIÓTICOS</option>
-            <option value="ANTIVIRAIS">ANTIVIRAIS</option>
-            <option value="ANTIFÚNGICOS">ANTIFÚNGICOS</option>
-            <option value="ANTIDEPRESSIVOS">ANTIDEPRESSIVOS</option>
-            <option value="ANSIOLÍTICOS">ANSIOLÍTICOS</option>
-            <option value="ANTIPSICÓTICOS">ANTIPSICÓTICOS</option>
-            <option value="ANTIHISTAMÍNICOS">ANTIHISTAMÍNICOS</option>
-            <option value="ANTIHIPERTENSIVOS">ANTIHIPERTENSIVOS</option>
-            <option value="DIURÉTICOS">DIURÉTICOS</option>
-            <option value="IMUNOSSUPRESSORES">IMUNOSSUPRESSORES</option>
-            <option value="OUTROS">OUTROS</option>
-          </select>
-        </div>
-        <div class="w-50">
-          <label for="dataInput">Validade</label>
-          <input type="date" id="dataInput" class="form-control w-50" min="<?php echo date('Y-m-d'); ?>" placeholder="Selecione uma data...">
-        </div>
-      </div>
-      <input type="submit" value="SALVAR" class="btn btn-success btn-lg mt-4 ">
-    </form>
-  </section>
+    <div id="nav_bar_padrao" style="margin-bottom: 100px;"></div>
+    <section class="border border-3 rounded p-5" style="border: rgba(255, 0, 0, .5);">
+        <h1 style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;">CADASTRO DE PRODUTOS</h1>
 
-  <script>
-    fetch('nav_bar_padrao.html')
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById('nav_bar_padrao').innerHTML = data;
-      });
-  </script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+        <form action="cadastroProduto_action.php" method="post">
+            <div class="mb-2 w-75">
+                <label for="nameInput">Nome do produto</label>
+                <input type="text" class="form-control" name="nome" required>
+            </div>
+            <div class="mb-2 d-flex col">
+                <div class="w-50">
+                    <label for="precoInput">Preço</label>
+                    <!-- Corrigido type="float" para type="number" -->
+                    <input type="number" id="precoInput" name="precoInput" step="0.01" min="0.01" class="form-control w-50" required>
+                </div>
+                <div class="w-50">
+                    <label for="quantidadeInput">Quantidade</label>
+                    <input type="number" id="quantidadeInput" name="quantidadeInput" step="1" min="1" class="form-control w-50" required>
+                </div>
+            </div>
+            <div class="mb-2 d-flex col">
+                <div class="w-50">
+                    <label for="categoriaSelect">Categoria</label>
+                    <select name="categoriaSelect" id="categoriaSelect" class="form-control w-50">
+                        <option value="" disabled selected>Selecione...</option>
+                        <option value="ANALGÉSICOS">ANALGÉSICOS</option>
+                        <option value="ANTI-INFLAMATÓRIOS">ANTI-INFLAMATÓRIOS</option>
+                        <option value="ANTIBIÓTICOS">ANTIBIÓTICOS</option>
+                        <option value="ANTIVIRAIS">ANTIVIRAIS</option>
+                        <option value="ANTIFÚNGICOS">ANTIFÚNGICOS</option>
+                        <option value="ANTIDEPRESSIVOS">ANTIDEPRESSIVOS</option>
+                        <option value="ANSIOLÍTICOS">ANSIOLÍTICOS</option>
+                        <option value="ANTIPSICÓTICOS">ANTIPSICÓTICOS</option>
+                        <option value="ANTIHISTAMÍNICOS">ANTIHISTAMÍNICOS</option>
+                        <option value="ANTIHIPERTENSIVOS">ANTIHIPERTENSIVOS</option>
+                        <option value="DIURÉTICOS">DIURÉTICOS</option>
+                        <option value="IMUNOSSUPRESSORES">IMUNOSSUPRESSORES</option>
+                        <option value="OUTROS">OUTROS</option>
+                    </select>
+                </div>
+                <div class="w-50">
+                    <label for="dataInput">Validade</label>
+                    <input type="date" id="dataInput" name="dataInput" class="form-control w-50" min="<?php echo date('Y-m-d'); ?>" placeholder="Selecione uma data...">
+                </div>
+            </div>
+            <input type="submit" value="SALVAR" class="btn btn-success btn-lg mt-4 ">
+        </form>
+    </section>
+
+    <script>
+        fetch('nav_bar_padrao.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('nav_bar_padrao').innerHTML = data;
+            });
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- cadastroProduto_action.php
+++ cadastroProduto_action.php
@@ -1,8 +1,25 @@
-<?php 
+<?php
 include "crudEstoque.php";
-///////////////////////mudar isso depois
-$p = new produtos(null ,"teste", 6.66, 666, "ANALGÉSICOS", date('Y-m-d'));
-print_r($p);
-$teste = $p->cadastrarProduto($p);
 
+// Verificando se o método de requisição é POST
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Usando o operador de coalescência nula para evitar avisos de chave indefinida
+    $nome = $_POST['nome'] ;
+    $preco = $_POST['precoInput'] ;
+    $quantidade = $_POST['quantidadeInput'] ;
+    $categoria = $_POST['categoriaSelect'] ;
+    $data_validade = $_POST['dataInput'] ;
+
+    // Verifique se os valores obrigatórios foram fornecidos
+    if ($nome && $preco && $quantidade && $categoria && $data_validade) {
+        $p = new produtos(null, $nome, $preco, $quantidade, $categoria, $data_validade);
+        $teste = $p->cadastrarProduto($p);
+    } else {
+        // Trate o erro, se necessário (ex.: redirecionar ou exibir mensagem)
+        echo "Por favor, preencha todos os campos obrigatórios.";
+    }
+} else {
+    // Se não for uma requisição POST, redirecionar ou exibir uma mensagem
+    echo "Método não permitido.";
+}
 ?>
